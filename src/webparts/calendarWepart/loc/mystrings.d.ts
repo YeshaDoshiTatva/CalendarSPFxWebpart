53540b4a1f6268bc90685d19f54e8cbcd8c938ba
YeshaDoshiTatva <yesha.doshi@tatvasoft.com>
2020-11-12 13:39:04 +0530

    commit

---
+++ src/webparts/calendarWepart/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ICalendarWepartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'CalendarWepartWebPartStrings' {
+  const strings: ICalendarWepartWebPartStrings;
+  export = strings;
+}
